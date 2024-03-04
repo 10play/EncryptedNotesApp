8a945c20fe64222610772d0dc1e38ff4722d9a68
Amir Angel <36531255+17Amir17@users.noreply.github.com>
2024-03-04 15:48:34 +0200

    add static history

--- src/Editor/EditorHeader.tsx
+++ src/Editor/EditorHeader.tsx
@@ -1,4 +1,4 @@
-import {EditorBridge, useBridgeState} from '@10play/tentap-editor';
+import {EditorBridge, Images, useBridgeState} from '@10play/tentap-editor';
 import React from 'react';
 import styled from 'styled-components/native';
 import {StyledText} from '../Components/StyledText';
@@ -12,17 +12,36 @@ const HeaderContainer = styled.View`
   height: 44px;
   align-items: center;
   border-bottom-width: 1px;
+  gap: 12px;
   border-bottom-color: ${props => props.theme['--border-primary']};
   flex-direction: row;
-  gap: 12px;
+  justify-content: space-between;
+`;
+const TitleAndChevron = styled.View`
+  flex-direction: row;
+  align-items: center;
 `;
 const BackChevron = styled.Image`
   width: 24px;
   height: 24px;
   tint-color: ${props => props.theme['--text-primary']};
 `;
+const HistoryContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+  padding: 12px;
+  gap: 4px;
+`;
 const HeaderTitle = styled(StyledText)`
   font-size: 14px;
+`;
+const HistoryIcon = styled.Image.attrs(({theme}) => {
+  return {
+    tintColor: theme['--text-primary'],
+  };
+})`
+  width: 24px;
+  height: 24px;
 `;
 
 interface EditorHeaderProps {
@@ -35,12 +54,36 @@ export const EditorHeader = ({editor, initialTitle}: EditorHeaderProps) => {
 
   return (
     <HeaderContainer>
-      <TouchableOpacity onPress={() => navigationRef.goBack()}>
-        <BackChevron source={chevron} />
-      </TouchableOpacity>
-      <HeaderTitle>
-        {editorState.isReady ? title || initialTitle : initialTitle}
-      </HeaderTitle>
+      <TitleAndChevron>
+        <TouchableOpacity onPress={() => navigationRef.goBack()}>
+          <BackChevron source={chevron} />
+        </TouchableOpacity>
+        <HeaderTitle>
+          {editorState.isReady ? title || initialTitle : initialTitle}
+        </HeaderTitle>
+      </TitleAndChevron>
+      <HistoryContainer>
+        <TouchableOpacity
+          onPress={() => editor.undo()}
+          disabled={!editorState.canUndo}>
+          <HistoryIcon
+            source={Images.undo}
+            style={{
+              opacity: editorState.canUndo ? 1 : 0.5,
+            }}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => editor.redo()}
+          disabled={!editorState.canRedo}>
+          <HistoryIcon
+            source={Images.redo}
+            style={{
+              opacity: editorState.canRedo ? 1 : 0.5,
+            }}
+          />
+        </TouchableOpacity>
+      </HistoryContainer>
     </HeaderContainer>
   );
 };
